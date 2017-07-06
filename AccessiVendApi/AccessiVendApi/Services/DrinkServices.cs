cfea92f754a22300f5cf844b674c8dce7cdae6f0
Joshua Wetzel <joshua.wetzel@notrs.com>
2017-07-06 10:30:22 -0500

    drink types and drink orders
    drink types and drink orders

--- AccessiVendApi/AccessiVendApi/Services/DrinkServices.cs
+++ AccessiVendApi/AccessiVendApi/Services/DrinkServices.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AccessiVendApi.DB;
+using AccessiVendApi.DB.Tables;
+using AccessiVendApi.ViewModels;
 
 namespace AccessiVendApi.Services
 {
@@ -13,6 +15,83 @@ namespace AccessiVendApi.Services
         public DrinkServices(CoreContext context)
         {
             _context = context;
+        }
+
+        public DrinkType NewDrinkType(DrinkType type)
+        {
+            try
+            {
+                _context.DrinkTypes.Add(type);
+                _context.SaveChanges();
+                return type;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            
+        }
+
+        public DrinkType UpdateDrinkType(DrinkType type)
+        {
+            try
+            {
+                _context.DrinkTypes.Update(type);
+                _context.SaveChanges();
+                return type;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+           
+        }
+
+        public List<DrinkOrder> ListAllDrinkOrders()
+        {
+            return _context.DrinkOrders.Where(order => order != null).ToList();
+        }
+
+        public List<DrinkOrder> ListDrinkOrdersByUserName(string name)
+        {
+            return _context.DrinkOrders.Where(order => order.User.Name.Contains(name)).ToList();
+        }
+
+        public List<DrinkOrder> ListDrinkOrdersByDrinkTypeName(string name)
+        {
+            return _context.DrinkOrders.Where(order => order.DrinkType.Description.Contains(name)).ToList();
+        }
+
+        public List<DrinkOrder> ListOfUnpaidDrinkOrders()
+        {
+            return _context.DrinkOrders.Where(order => !order.Paid).ToList();
+        }
+        public List<DrinkOrder> ListOfUnpaidDrinkOrdersByName(string name)
+        {
+            return _context.DrinkOrders.Where(order => !order.Paid && order.User.Name.Contains(name)).ToList();
+        }
+        public DrinkOrder BuyDrink(BuyDrink order)
+        {
+            try
+            {
+                var user = _context.Users.First(person => person.Name.Contains(order.Name));
+                var drinkType = _context.DrinkTypes.First(type => type.Description.Contains(order.DrinkType));
+                var drinkOrder = new DrinkOrder()
+                {
+                    DrinkType = drinkType,
+                    DrinkTypeId = drinkType.Id,
+                    User = user,
+                    UserId = user.Id
+                };
+                _context.DrinkOrders.Add(drinkOrder);
+                _context.SaveChanges();
+                return drinkOrder;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            
         }
     }
 }
